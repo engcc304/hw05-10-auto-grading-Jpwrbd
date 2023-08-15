0bdc789389bf978ddfb0b91d7a2e44fdc6355002
Jpwrbd <jpwrbd456@gmail.com>
2023-08-16 00:23:26 +0700

    up

--- Homework/HW05/hw05-10-auto-grading-without-if-else.cpp
+++ Homework/HW05/hw05-10-auto-grading-without-if-else.cpp
@@ -1,8 +1,8 @@
 /*
     ผู้ใช้กรอกค่า "คะแนนดิบ" เข้ามาในระบบเพื่อต้องการตรวจสอบเกรดในรายวิชา Programming ซึ่งมีเกณฑ์การให้คะแนนคือ F อยู่ในช่วงคะแนนน้อยกว่า 50 , D อยู่ในช่วงระหว่าง 50 ถึง 55 , D+ อยู่ในช่วงระหว่าง 55 ถึง 60 , C อยู่ในช่วงระหว่าง 60 ถึง 65 , C+ อยู่ในช่วงระหว่าง 65 ถึง 70 , B อยู่ในช่วงระหว่าง 70 ถึง 75 , B+ อยู่ในช่วงระหว่าง 75 ถึง 80 , A อยู่ในช่วงคะแนนมากกว่า 80 ขึ้นไป
-    #TODO #20 : fix this code without if else syntax.
+    #TODO #20 : fix this code without if else syntax. 
     (กำหนดให้ข้อนี้ใช้คำสั่ง switch case ได้เท่านั้น)
-
+    
     Test case:
         enter score :
             80
@@ -28,47 +28,43 @@
         F !
 */
 #include <stdio.h>
-int main()
-{
+int main() {
     int score;
-    printf("Test case:\n");
-    printf("\tenter score:\n\t\t");
-    scanf("t%d", &score);
-    printf("\tOutput:\n");
-    switch (score / 5)
-    {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-        printf("\t\tF !\n");
-        break;
-    case 10:
-        printf("\t\tD !\n");
-        break;
-    case 11:
-        printf("\t\tD+ !\n");
-        break;
-    case 12:
-        printf("\t\tC !\n");
-        break;
-    case 13:
-        printf("\t\tC+ !\n");
-        break;
-    case 14:
-        printf("\t\tB !\n");
-        break;
-    case 15:
-        printf("\t\tB+ !\n");
-        break;
-    default:
-        printf("\t\tA !\n");
-        break;
+    printf("enter score:\n");
+    scanf("%d", &score);
+    switch (score / 5) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            printf("F !\n");
+            break;
+        case 10:
+            printf("D !\n");
+            break;
+        case 11:
+            printf("D+ !\n");
+            break;
+        case 12:
+            printf("C !\n");
+            break;
+        case 13:
+            printf("C+ !\n");
+            break;
+        case 14:
+            printf("B !\n");
+            break;
+        case 15:
+            printf("B+ !\n");
+            break;
+        default:
+            printf("A !\n");
+            break;
     }
     return 0;
 }
